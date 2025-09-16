444a2cf2f9dc727ce6246aee5c173908db9903df
Ngọ Đức Cảnh <ngoduccanh19@gmail.com>
2025-09-16 15:21:29 +0700

    feat: add ticket movie slider background image

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -17,6 +17,36 @@
         class="w-full h-auto mx-auto"
       >
     </UCarousel>
+    <div class="marquee-wrapper bookmt-90 w-full">
+      <div class="marquee-inner w-full">
+        <div class="common-slider w-full">
+          <div class="marquee-items">
+            <div
+              v-for="n in 2"
+              :key="n"
+              class="flex"
+            >
+              <div
+                v-for="(item, index) in imagesList"
+                :key="index"
+                class="item"
+              >
+                <div class="img-wrap">
+                  <img
+                    :src="item"
+                    width="220"
+                    height="220"
+                    loading="lazy"
+                    alt="movie"
+                    class="img-fluid"
+                  >
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -32,8 +62,86 @@ const items = [
   '/images/phim-2.png',
   '/images/phim-1.png'
 ]
+const imagesList = [
+  '/images/img-1.png',
+  '/images/img-2.png',
+  '/images/img-3.png',
+  '/images/img-4.png',
+  '/images/img-5.png',
+  '/images/img-6.png',
+  '/images/img-7.png',
+  '/images/img-8.png'
+]
 </script>
 
 <style scoped>
+.marquee-wrapper {
+  overflow: hidden;
+  width: 100%;
+  line-height: 0;
+}
 
+.bookmyticket-wrapper .bookmt-90 {
+  margin-top: 90px;
+}
+
+.marquee-inner {
+  display: flex;
+  align-items: center;
+  white-space: nowrap;
+  width: 100%;
+  background: url(/images/ticket-slider-bg.png) no-repeat;
+  height: 310px;
+  background-size: cover;
+}
+
+.common-slider {
+  display: flex;
+  flex-wrap: nowrap;
+  width: 100%;
+}
+
+/* Chạy liên tục */
+.marquee-items {
+  display: flex;
+  flex-wrap: nowrap;
+  animation: moviemarqueeLeft 60s linear infinite;
+}
+
+.item {
+  width: 220px;
+  height: 220px;
+  margin: 0 10px;
+  flex-shrink: 0;
+}
+
+.item .img-wrap img {
+  width: 220px;
+  aspect-ratio: 1;
+  border-radius: 6px;
+  filter: grayscale(100%);
+  transition: filter 0.3s;
+}
+
+.item .img-wrap img:hover {
+  filter: grayscale(0%);
+}
+
+.img-fluid {
+  max-width: 100%;
+  height: auto;
+}
+
+.marquee-items .item:hover .img-wrap img {
+  filter: none;
+}
+
+@keyframes moviemarqueeLeft {
+  0% {
+    transform: translateX(0);
+  }
+  100% {
+    transform: translateX(-50%);
+  }
+}
 </style>
