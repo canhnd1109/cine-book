8a4de4860e116859f18d521bc987b3786ab838cb
Ngọ Đức Cảnh <ngoduccanh19@gmail.com>
2025-09-25 23:51:12 +0700

    refactor: centralize API request handling in BaseService by introducing handleRequest method to streamline error management and reduce code duplication

--- app/services/base.service.ts
+++ app/services/base.service.ts
@@ -1,6 +1,7 @@
 import type { NitroFetchRequest } from 'nitropack'
 import { useNuxtApp } from '#app'
 import type { ExtendedFetchOptions } from '~/types/http.type'
+
 type RequestBody = Record<string, unknown> | FormData | string
 
 export default class BaseService {
@@ -14,14 +15,22 @@ export default class BaseService {
     return useNuxtApp().$http as <T>(url: NitroFetchRequest, options?: ExtendedFetchOptions) => Promise<T>
   }
 
+  private async handleRequest<T>(url: string, options: ExtendedFetchOptions): Promise<T> {
+    try {
+      return await this.api<T>(`${this.prefix}${url}`, options)
+    } catch (error: any) {
+      throw {
+        error: true,
+        data: error?.data || null
+      }
+    }
+  }
+
   async get<T, Q extends Record<string, string | number | boolean | null | undefined> = Record<string, never>>(
     url: string,
     params?: Q
   ): Promise<T> {
-    return this.api<T>(`${this.prefix}${url}`, {
-      method: 'get',
-      query: params
-    })
+    return this.handleRequest<T>(url, { method: 'get', query: params })
   }
 
   async post<T, B extends Record<string, unknown> | FormData | string | undefined = undefined>(
@@ -29,34 +38,21 @@ export default class BaseService {
     body?: B,
     config?: { headers?: Record<string, string> }
   ): Promise<T> {
-    return this.api<T>(`${this.prefix}${url}`, {
-      method: 'post',
-      body,
-      ...config
-    })
+    return this.handleRequest<T>(url, { method: 'post', body, ...config })
   }
 
   async put<T, B extends Record<string, unknown> | FormData | undefined = undefined>(url: string, body?: B): Promise<T> {
-    return this.api<T>(`${this.prefix}${url}`, {
-      method: 'put',
-      body
-    })
+    return this.handleRequest<T>(url, { method: 'put', body })
   }
 
   async patch<T, B extends Record<string, unknown> | FormData | undefined = undefined>(url: string, body?: B): Promise<T> {
-    return this.api<T>(`${this.prefix}${url}`, {
-      method: 'patch',
-      body
-    })
+    return this.handleRequest<T>(url, { method: 'patch', body })
   }
 
   async delete<T, Q extends Record<string, string | number | boolean | null | undefined> = Record<string, never>>(
     url: string,
     params?: Q
   ): Promise<T> {
-    return this.api<T>(`${this.prefix}${url}`, {
-      method: 'delete',
-      query: params
-    })
+    return this.handleRequest<T>(url, { method: 'delete', query: params })
   }
 }
