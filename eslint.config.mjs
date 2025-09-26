f588e310b3a7549ab2b0d1549f31230ba34c0a82
Ngọ Đức Cảnh <ngoduccanh19@gmail.com>
2025-09-26 08:39:09 +0700

    chore: update ESLint configuration to include no-explicit-any rule and maintain existing no-unused-vars settings for improved code quality

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,16 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt(
+export default withNuxt({
   rules: {
-    '@typescript-eslint/no-unused-vars': ['warn', {
-      argsIgnorePattern: '^_',
-      varsIgnorePattern: '^_',      /
-      ignoreRestSiblings: true
-    }],
-  },
-)
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        ignoreRestSiblings: true
+      }
+    ]
+  }
+})
