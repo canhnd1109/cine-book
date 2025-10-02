1476b4000421c8cf8abf179a3b3ab92e442b7b9d
Ngọ Đức Cảnh <ngoduccanh19@gmail.com>
2025-10-02 14:14:01 +0700

    style: update background colors in main.css and adjust layout in genre and index pages for improved UI consistency

--- app/pages/admin/movies/index.vue
+++ app/pages/admin/movies/index.vue
@@ -3,9 +3,9 @@ definePageMeta({ layout: 'admin', middleware: ['admin'] })
 </script>
 
 <template>
-  <div class="dark:bg-bg-primary-dark bg-bg-light rounded-2xl mr-6">
+  <div class="space-y-4">
     <MoviesTabs />
-    <div class="p-4">
+    <div class="p-4 dark:bg-bg-primary-dark bg-bg-light rounded-2xl mr-6">
       <h2>Movies Tab</h2>
     </div>
   </div>
