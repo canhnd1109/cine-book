4575a73229a0559802a84af8a0708a7f1cc68410
Ngọ Đức Cảnh <ngoduccanh19@gmail.com>
2025-10-03 10:11:49 +0700

    refactor: simplify search functionality in GenreFilter component by replacing debounce with direct search trigger and updating input component

---
+++ app/components/base/BaseInput.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { debounce } from 'lodash-es'
+
+const props = defineProps<{ modelValue: string }>()
+
+const emits = defineEmits<{
+  (e: 'update:modelValue', value: string): void
+  (e: 'search'): void
+}>()
+
+const { t } = useI18n()
+const inputValue = ref(props.modelValue)
+
+watch(
+  () => props.modelValue,
+  val => {
+    inputValue.value = val
+  }
+)
+
+const handleSearch = debounce(() => {
+  emits('search')
+}, 400)
+
+const onInput = (val: string) => {
+  inputValue.value = val
+  emits('update:modelValue', val)
+  handleSearch()
+}
+</script>
+
+<template>
+  <UInput
+    :model-value="inputValue"
+    icon="i-lucide-search"
+    size="lg"
+    variant="outline"
+    loading-icon="i-lucide-loader"
+    :placeholder="t('search')"
+    @update:model-value="onInput"
+  />
+</template>
--- app/components/genre/GenreFilter.vue
+++ app/components/genre/GenreFilter.vue
@@ -1,28 +1,18 @@
 <script setup lang="ts">
-import { debounce } from 'lodash-es'
 const emits = defineEmits<{
   search: []
   add: [value: boolean]
 }>()
 const search = defineModel<string>('search', { default: '' })
 const { t } = useI18n()
-
-const handleSearch = debounce(() => {
-  emits('search')
-}, 400)
+const doSearch = () => {
+  console.log('Search triggered:', search.value)
+}
 </script>
 
 <template>
   <div class="flex justify-between items-center">
-    <UInput
-      v-model="search"
-      icon="i-lucide-search"
-      size="lg"
-      variant="outline"
-      loading-icon="i-lucide-loader"
-      :placeholder="t('search')"
-      @update:model-value="handleSearch"
-    />
+    <BaseInput v-model="search" @search="doSearch" />
     <BaseButton :text="t('add')" variant="solid" class-name="rounded" @click="emits('add', true)" />
   </div>
 </template>
