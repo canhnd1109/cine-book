7a81f31d7be41829ca9509d52395fe85f2878dd6
Ngọ Đức Cảnh <ngoduccanh19@gmail.com>
2025-10-13 17:51:10 +0700

    feat: enhance cinema list component with loading and empty states, implement image hover effects, and improve layout for better user experience

--- app/components/cinema/List.vue
+++ app/components/cinema/List.vue
@@ -1,8 +1,79 @@
 <script setup lang="ts">
 const { cinemas } = useCinemaData()
+
+const { isFetching = false } = defineProps<{
+  isFetching?: boolean
+}>()
+const hoveredItem = ref<string | null>(null)
 </script>
+
 <template>
-  <div>{{ cinemas }}</div>
+  <BaseSkeletonCard v-if="isFetching" />
+  <BaseEmpty v-else-if="!cinemas.length" />
+  <div v-else class="grid-cols-5 gap-6 grid max-lg:grid-cols-4 max-md:grid-cols-3 max-sm:grid-cols-2">
+    <div v-for="item in cinemas" :key="item.id">
+      <BaseCard :item="item" :index="0" class="w-full">
+        <template #image>
+          <div class="flex items-center justify-center">
+            <div
+              class="relative flex items-center justify-center overflow-hidden rounded-lg w-full h-60"
+              @mouseenter="hoveredItem = item.id"
+              @mouseleave="hoveredItem = null"
+            >
+              <div class="image-container" :class="{ 'slide-active': hoveredItem === item.id }">
+                <img
+                  :src="item.urlImages[0]"
+                  :alt="item.name"
+                  class="first-image absolute top-0 left-0 h-full w-full object-cover max-sm:object-center"
+                />
+                <img
+                  v-if="item.urlImages.length > 1"
+                  :src="item.urlImages[1]"
+                  :alt="item.name"
+                  class="second-image absolute top-0 left-0 h-full w-full object-cover"
+                />
+              </div>
+            </div>
+          </div>
+        </template>
+
+        <template #content>
+          <p class="mt-2 line-clamp-2 text-center font-medium">
+            {{ item.name }}
+          </p>
+          <p>
+            <span class="text-[#90a1b9] text-sm">{{ $t('address') }}: </span>
+            <span>{{ item.province }} - {{ item.commune }} - {{ item.detailAddress }}</span>
+          </p>
+          <p>
+            <span class="text-[#90a1b9] text-sm">{{ $t('phone') }}: </span>
+            <span>{{ item.phone }}</span>
+          </p>
+          <p>
+            <span class="text-[#90a1b9] text-sm">{{ $t('description') }}: </span>
+            <span>{{ item.description }}</span>
+          </p>
+        </template>
+      </BaseCard>
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.first-image,
+.second-image {
+  transition: transform 0.4s ease;
+}
+
+.second-image {
+  transform: translateX(100%);
+}
+
+.slide-active .first-image {
+  transform: translateX(-100%);
+}
+
+.slide-active .second-image {
+  transform: translateX(0);
+}
+</style>
